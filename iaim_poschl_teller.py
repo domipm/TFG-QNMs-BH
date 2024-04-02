0a8ded8f6f194781defcdd3e5106aee6b15a518f
domipm <dpas602@gmail.com>
2024-04-02 12:34:25 +0200

    Fixed IAIM Poschl-Teller, copied to python code

--- iaim_poschl_teller.py
+++ iaim_poschl_teller.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #NUMBER OF ITERATIONS TO PERFORM
-N = 5
+N = 15
 
 #SYMBOLIC VARIABLES DEFINITIONS
 y = symbols("y", real=True)
@@ -33,18 +33,30 @@ D = np.zeros((N,N),dtype=object)
 C[:,0] = series_coeff(l0,y,0)
 D[:,0] = series_coeff(s0,y,0)
 
-for n in range(1,N):
-    for i in range(N-1):
-        C[i,n] = (i+1)*C[i+1,n-1] + D[i,n-1]
-        D[i,n] = (i+1)*D[i+1,n-1]
-        for k in range(0,i):
-            C[i,n] = C[i,n] + C[k,0]*C[i-k,n-1]
-            D[i,n] = D[i,n] + D[k,0]*C[i-k,n-1]
+#CALCULATE MATRICES C and D
+for n in range(0,N-1): #COLUMNS
+    for i in range(0,N): #ROWS
+        if (i+1 == N):
+            D[i,n+1] = 0
+            C[i,n+1] = D[i,n]
+        else:
+            D[i,n+1] = (i+1)*D[i+1,n]
+            C[i,n+1] = (i+1)*C[i+1,n]+D[i,n]
+        for k in range(0,i+1): #SUM INTERIOR
+            C[i,n+1] = C[i,n+1] + C[k,0]*C[i-k,n]
+            D[i,n+1] = D[i,n+1] + D[k,0]*C[i-k,n]
 
-d = D[0,N-1]*C[0,N-2] - D[0,N-2]*C[0,N-1]
+#SOLVE QUANTIZATION CONDITION
+d = D[0,n]*C[0,n-1] - D[0,n-1]*C[0,n]
+#d.expand()
 
-print(d)
-print("\n")
-print(solve(d,w))
+#FIND ROOTS OF POLYNOMIAL OF w (omega) (Using sympy, algebraic roots, might be slow, use numerical instead)
+sols = solve(d,w)
+print("All solutions:")
+print(sols)
+print("Filtered solutions:")
+for i in range(len(sols)):
+    if (re(sols[i]) > 0 and  im(sols[i]) < 0):
+        print("w_" + str(i+1) + " = " + str(sols[i]))
 
 exit()
