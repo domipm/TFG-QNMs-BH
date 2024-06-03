bc9f4eb130e7cd98379460b5783306d734e4cd5d
domipm <dpas602@gmail.com>
2024-06-03 10:49:32 +0200

    Added Poschl-Teller approximation for Regge-Wheeler potential

--- schwarzschild/aim_qnm_pt_approx.py
+++ schwarzschild/aim_qnm_pt_approx.py
@@ -9,43 +9,32 @@ import aim_qnm as aim
 # Complex variable
 I = sym.I
 # Number of iterations to perform
-n = 12
+n = 20
 
 # Symbolic variable definitions
 y = sym.symbols("y", real=True)
 w = sym.symbols("\omega")
 
-# Angular momentum, spin, and mass
-l = sym.symbols("l", real=True)
-s = sym.symbols("s", real=True)
-m = sym.symbols("m", real=True)
+# Fitted parameters
+# ( Correct parameters checked in ref. )
+a = 0.16345200812517632
+v = 0.15035326143963337
 
-# Numeric parameters (M=1, l=2, s=2)
-m_val = 1
-l_val = 2
-s_val = 2
-
-a = 1 / (m_val*sym.sqrt(27))
-v = l_val*(l_val + 1)
+#a = 1
+#v = 1/2
 
 # Asymptotically Flat Schwarzschild Initial Parameters (lambda_0 and s_0)
-l0 =  2*y*(1-I*w)/(1-y**2)
-s0 = (I*w-v)/(1-y**2) + (w**2*(1-a**2*y**2)/(a**2*(1-y**2)**2))
+l0 = (2*y*(1-I*w))/(1-y**2)
+s0 = (v-w*(w+I*a))/(a**2*(1-y**2))
 
-#Substitute
-#l0 = l0.subs(m,m_val).subs(l,l_val).subs(s,s_val)
-#s0 = s0.subs(m,m_val).subs(l,l_val).subs(s,s_val)
-
-# Find point where to substitute (maximum of potential)
-y0 = -( 1 - 2*m / ( 3*m/2 * 1/(l*(l+1)) * ( l*(l+1) - (1-s**2) + sym.sqrt( l**2*(l+1)**2 + 14/9*l*(l+1)*(1-s**2) + (1-s**2)**2 ) ) ) ).subs(m, m_val).subs(l, l_val).subs(s, s_val).evalf(75)
-
-# Max value: 0.39038820320220757320583970795269124209880828857421875
+# Find point where to substitute
+y0 = 0
 
 # AIM ALGORITHM
 
 start = time.time()
 
-# Create AIM solver object
+# Create AIM solver object ( Gives same results as IAIM )
 aim = aim.aim_solver(l0,s0,x=y,x0=y0,n_iter=n)
 aim.aim_init() # Initialize AIM algorithm with same parameters as AIM class object
 aim.aim_solve(solver="mpnum", display_all=False, print_delta=False)
@@ -55,8 +44,6 @@ stop = time.time()
 print("\nComputation time (AIM Method): ", str(stop-start) + "\n")
 
 # IAIM ALGORITHM
-
-exit()
 
 start = time.time()
 
